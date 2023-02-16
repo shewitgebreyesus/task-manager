f4db411051a55b3e53ac76db3389a8d85b22bafd
shewit <shewitgebreyesus76@gmail.com>
2023-02-16 11:11:11 +0300

    update

--- src/models/user.js
+++ src/models/user.js
@@ -76,7 +76,7 @@ userSchema.methods.toJSON = function () {
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
-=======
+======
     
 >>>>>>> abd8c3260cf2edc5488d39e1cd5e3520efc1305a
 
